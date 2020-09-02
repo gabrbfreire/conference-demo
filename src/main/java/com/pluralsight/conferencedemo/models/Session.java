5039eec17244df6b9e18bd81438b8b1d8f23dd8d
Gabriel Freire <gabrbfreire@gmail.com>
2020-09-02 15:45:04 -0300

    first commit

---
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -0,0 +1,72 @@
+package com.pluralsight.conferencedemo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+// JPA Entity
+// Cada Entity representa uma coluna em sua tabela
+@Entity(name = "sessions") // Nome da tabela no banco
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Session {
+
+    // Variáveis com mesmo nome dos campos na tabela
+    @Id // Identifica a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
+
+    @ManyToMany
+    @JoinTable(
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id"))
+    private List<Speaker> speakers;
+
+    public Session(){
+
+    }
+
+    public Long getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(Long session_id) {
+        this.session_id = session_id;
+    }
+
+    public String getSession_name() {
+        return session_name;
+    }
+
+    public void setSession_name(String session_name) {
+        this.session_name = session_name;
+    }
+
+    public String getSession_description() {
+        return session_description;
+    }
+
+    public void setSession_description(String session_description) {
+        this.session_description = session_description;
+    }
+
+    public Integer getSession_length() {
+        return session_length;
+    }
+
+    public void setSession_length(Integer session_length) {
+        this.session_length = session_length;
+    }
+
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
+    }
+}
