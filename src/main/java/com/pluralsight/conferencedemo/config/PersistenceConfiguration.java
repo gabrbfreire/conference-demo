d85e96b4be9f2f5dbda2cfadc626a827ae36091c
Gabriel Freire <gabrbfreire@gmail.com>
2020-09-02 17:04:35 -0300

    configs

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,17 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:mysql://${MYSQL_HOST:localhost}:3306/conference_demo?useTimezone=true&serverTimezone=UTC");
+        return builder.build();
+    }
+}
